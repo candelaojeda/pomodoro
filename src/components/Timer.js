1cf3c357b8341135d28da9258fa16c0cdfdb9345
candelaojeda <ojedacandelaco@gmail.com>
2022-02-28 16:16:36 -0300

    added components buttons in component Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,8 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import PauseButton from "./PauseButton";
+import PlayButton from "./PlayButton";
+import SettingsButton from "./SettingsButton";
 
 const red = "#f54e4e";
 const green = "#4aec8c";
@@ -16,11 +19,14 @@ function Timer() {
           tailColor: "rgba(255,255,255,.2)",
         })}
       />
-      <div>
-          
+      <div style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <SettingsButton />
       </div>
     </div>
   );
 }
-
 export default Timer;
