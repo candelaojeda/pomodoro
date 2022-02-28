be4f51db9e622f1842f11a7f4814fd45920fcbdb
candelaojeda <ojedacandelaco@gmail.com>
2022-02-28 19:06:27 -0300

    added settings with useContext and install react slider

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,19 +1,37 @@
 import ReactSlider from "react-slider";
 import "../slider.css";
+import SettingsContext from "./SettingsContext";
+import { useContext } from "react";
+import BackButton from "./BackButton";
 
 function Settings() {
+  const settingsInfo = useContext(SettingsContext);
+
   return (
     <div style={{ textAlign: "left" }}>
-      <label>work minutes :</label>
+      <label>work minutes: {settingsInfo.workMinutes}:00</label>
       <ReactSlider
         className="slider"
         thumbClassName="thumb"
         trackClassName="track"
-        value={45}
+        value={settingsInfo.workMinutes}
+        onChange={(newValue) => settingsInfo.setWorkMinutes(newValue)}
         min={1}
         max={120}
       />
-      <label>break minutes:</label>
+      <label>break minutes: {settingsInfo.breakMinutes}:00</label>
+      <ReactSlider
+        className="slider green"
+        thumbClassName="thumb"
+        trackClassName="track"
+        value={settingsInfo.breakMinutes}
+        onChange={(newValue) => settingsInfo.setBreakMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+      </div>
     </div>
   );
 }
