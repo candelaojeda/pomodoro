c795445467b82f39b272541fa119542de40ff59f
candelaojeda <ojedacandelaco@gmail.com>
2022-02-28 21:47:33 -0300

    added main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
+import Timer from "./components/Timer";
+import Settings from "./components/Settings";
+import { useState } from "react";
+import SettingsContext from "./components/SettingsContext";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
   );
 }
 
